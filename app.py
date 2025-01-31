1ebb831ffc42000f048c48584dffff40fb7dc6e1
Pradeep031005 <pradeep22td0705@svcet.ac.in>
2025-01-31 13:07:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/data', methods=['GET', 'POST'])
+def handle_data():
+    if request.method == 'POST':
+        data = request.json
+        return {"message": "Data received", "data": data}
+    return {"message": "Send a POST request with JSON data"}
+
+if __name__ == '__main__':
+    app.run(debug=True)
